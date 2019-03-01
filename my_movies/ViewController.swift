b761b9af5874b7fdeca668c08f9a7398f3058cb3
almirpask <almirpask@hotmail.com>
2019-02-28 23:15:59 -0300

    Initial Commit

---
+++ my_movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  my_movies
+//
+//  Created by Almir Santos on 28/02/19.
+//  Copyright © 2019 Almir Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

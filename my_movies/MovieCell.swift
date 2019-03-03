e284314f88f5d59b5316a6dfa7143f7300ef6940
almirpask <almirpask@hotmail.com>
2019-03-03 10:38:28 -0300

    Create MovieCell class

---
+++ my_movies/MovieCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCell.swift
+//  my_movies
+//
+//  Created by Almir Santos on 02/03/19.
+//  Copyright © 2019 Almir Santos. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var moviewImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+}

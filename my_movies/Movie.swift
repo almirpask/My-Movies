d8b6dbea661644bb88de3d300603490aeb55704f
almirpask <almirpask@hotmail.com>
2019-03-03 10:37:49 -0300

    Create Movie class

---
+++ my_movies/Movie.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class Movie {
+    var title: String!
+    var description: String!
+    var image: UIImage!
+    
+    init(title:String,description:String, image:UIImage) {
+        self.title = title
+        self.description = description
+        self.image = image
+    }
+}
